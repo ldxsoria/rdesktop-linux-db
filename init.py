989784bb19032fddc41a4fa638188a914614df30
ldxsoria <ldxsoria@gmail.com>
2022-09-22 10:21:08 -0500

    Modularizacion al 50%

--- init.py
+++ init.py
@@ -12,13 +12,28 @@ controller.createDB(_PATH)
 controller.createTable(_PATH)
 
 def equipos_registrados():
-    pass
+    dic_registros = {} #Creo un diccionario, para usar # como un identificador
+    n = 0 # llave del diccionario
+    equipos = ServersDao.selecionar()
+    for equipo in equipos:
+        n += 1
+        print(f'{n}. {equipo[0]}') #Imprimo la lista de objetos registrados con un numero correlativo
+        dic_registros[n] = equipo[0] #Aqui
+    return dic_registros
+
+
+def ejecutar_conexion(opcion):
+    name = dic_equipos[opcion] #Chat name
+    server = Servers(name=name) #Created object
+    remote_server_db = ServersDao.buscar(server) #Search object by name selected
+    remote_server = Servers(ip=remote_server_db[1],username=remote_server_db[2], password=remote_server_db[3])
+    remote_conn = ServersDao.conectar(remote_server)
+    #print(remote_server)
 
 opcion = None
-os.system('clear')
 while opcion != 4:
     #Este sera el menu principal
-    
+    os.system('clear')
     print('RDESKTOP DATABASE\nOpciones:')
     print('1) Conectarme a un equipo')
     print('2) Agregar una nueva conexion')
@@ -26,38 +41,22 @@ while opcion != 4:
     print('4) Salir')
     opcion = int(input('Ingresa tu opción (1-3) > '))
 
-    #Este sera el menu de -> Listar equipos
+    #Menu 1
     if opcion == 1:
-
         os.system('clear')
-        dic_equipos = {} #Creo un diccionario, para usar # como un identificador
-        n = 0 # llave del diccionario
         print('Los equipos registrados hasta ahora son:')
-
-        equipos = ServersDao.selecionar()
-        for equipo in equipos:
-            n += 1
-            print(f'{n}. {equipo[0]}') #Imprimo la lista de objetos registrados con un numero correlativo
-            dic_equipos[n] = equipo[0] #Aqui 
-
-        opcion = int(input('Ingresa el número del equipo a conectar (0 para salir)> '))
-        if opcion != 0:
-            print(dic_equipos)
-            name = dic_equipos[opcion] #Chat name
-            #rdesktop %s -u %s -p %s
-            server = Servers(name=name) #Created object
-            remote_server_db = ServersDao.buscar(server) #Search object by name selected
-            remote_server = Servers(ip=remote_server_db[1],username=remote_server_db[2], password=remote_server_db[3])
-            remote_conn = ServersDao.conectar(remote_server)
-            print(remote_server)
-            #remote_server = Servers(name=dic_equipos[opcion])
-            #conectar = ServersDao.conectar(remote_server)
+        dic_equipos = equipos_registrados()
+        sub_opcion = int(input('Ingresa el número del equipo a conectar (0 para salir) > '))
+        if sub_opcion != 0:
+            ejecutar_conexion(sub_opcion)
         else:
             pass
-        os.system('clear')
 
+    #Menu 2
     elif opcion == 2:
-        #os.system('clear')
+        os.system('clear')
+        print('Los equipos registrados hasta ahora son:')
+        equipos_registrados()
         name = input('Ingresa el nombre del equipo >')
         ip = input(f'Ingresa la IP de {name} >')
         username = input('Ingresa el usuario > ')
@@ -65,16 +64,22 @@ while opcion != 4:
         newRegistro = Servers(name, ip, username, password)
         ServersDao.intertar(newRegistro)
         print('Servidor registrado')
-        os.system('clear')
 
+
+    #Menu 3
     elif opcion == 3:
         os.system('clear')
-        name = input('Ingresa el nombre de la conexion a actualizar >')
-        new_ip = input(f'Ingresa la IP de {name} >')
-        new_username = input('Ingresa el usuario > ')
-        new_password = input('Ingresa la constraseña >')
-        registro = Servers(name,new_ip,new_username,new_password)
-        ServersDao.actualizar(registro)
+        equipos_registrados()
+        sub_opcion = int(input('Ingresa el número de la conexcion a actualizar (0 para salir) > '))
+        if sub_opcion != 0:
+
+            new_ip = input(f'Ingresa la IP de {sub_opcion} >')
+            new_username = input('Ingresa el usuario > ')
+            new_password = input('Ingresa la constraseña >')
+            registro = Servers(name,new_ip,new_username,new_password)
+            ServersDao.actualizar(registro)
+        else:
+            pass
 
     elif opcion == 4:
         print('Bye')
