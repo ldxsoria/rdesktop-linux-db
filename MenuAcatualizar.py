466ced57da0c955032662070f011e21c1a939fe3
ldxsoria <ldxsoria@gmail.com>
2022-09-22 13:33:32 -0500

    Opcion "3)ACTUALIZAR" totalmente reestrucutra y se creo una clases para su menu de preguntas

---
+++ MenuAcatualizar.py
@@ -0,0 +1,8 @@
+class MenuAcatualizar:
+
+    def __init__(self,numero, pregunta1, pregunta2, respuesta):
+        self.numero = numero
+        self.pregunta1 = pregunta1
+        self.pregunta2 = pregunta2
+        self.respuesta = respuesta
+    
--- init.py
+++ init.py
@@ -1,6 +1,7 @@
 import controller
 from ServersDao import ServersDao
 from Servers import Servers
+from MenuAcatualizar import MenuAcatualizar as Etapa
 import logging as log
 import os
 import pathlib
@@ -32,6 +33,42 @@ def ejecutar_conexion(sub_opcion):
     remote_conn = ServersDao.conectar(remote_server)
     #print(remote_server)
 
+def actualizar(data_actual):
+
+    pregunta_ip = Etapa(1, 'la ip','la nueva ip', data_actual.ip)
+    pregunta_username = Etapa(2, 'el usuario','el nuevo usuario', data_actual.username)
+    pregunta_password = Etapa(3, 'la contraseña', 'la nueva contraseña', data_actual.password)
+    tupla = (pregunta_ip, pregunta_username, pregunta_password)
+    eleccion = 'ejecutar'
+
+    etapa = 0
+
+    
+    for e in range(0,3):
+
+        print(f'¿Deseas actualizar {tupla[etapa].pregunta1} > {tupla[etapa].respuesta} ?\n1. Sí\n2. No')
+        #Actualizar IP?
+        ip = int(input('> '))
+        if ip == 1:
+            tupla[etapa].respuesta = input(f'Ingresa {tupla[etapa].pregunta2} de >')
+            print(tupla[etapa].respuesta)
+            etapa += 1
+        elif ip == 2:
+            #new_ip = data_actual.ip
+            #print(tupla[etapa])
+            etapa += 1
+        else:
+            print('OPCION INCORRECTA')
+
+    print(f'{tupla[0].respuesta}, {tupla[1].respuesta}, {tupla[2].respuesta}')
+
+    #Almacena los cambios en un nuevo objeto "Servers"
+    data_actualizada = Servers(data_actual.name, tupla[0].respuesta, tupla[1].respuesta, tupla[2].respuesta)
+    #Realiza un execute para guardas los cambios
+    ServersDao.actualizar(data_actualizada)
+#FIN actualizar()
+
+
 opcion = None
 while opcion != 4:
     #Este sera el menu principal
@@ -55,18 +92,23 @@ while opcion != 4:
         else:
             pass
 
-    #Menu 2
+    #Menu 2 REGISTRAR
     elif opcion == 2:
         os.system('clear')
         print('Los equipos registrados hasta ahora son:')
         equipos_registrados()
-        name = input('Ingresa el nombre del equipo >')
-        ip = input(f'Ingresa la IP de {name} >')
-        username = input('Ingresa el usuario > ')
-        password = input('Ingresa la constraseña >')
-        newRegistro = Servers(name, ip, username, password)
-        ServersDao.intertar(newRegistro)
-        print('Servidor registrado')
+        print('INGRESA "0" PARA REGRESAR AL MENU\nPresiona "ENTER" para continuar')
+        sub_opcion = input('> ')
+        if sub_opcion == '0':
+            pass
+        else:
+            name = input('Ingresa el nombre del equipo >')
+            ip = input(f'Ingresa la IP de {name} >')
+            username = input('Ingresa el usuario > ')
+            password = input('Ingresa la constraseña >')
+            newRegistro = Servers(name, ip, username, password)
+            ServersDao.intertar(newRegistro)
+            print('Servidor registrado')
 
 
     #Menu 3 ACTUALIZAR
@@ -80,45 +122,12 @@ while opcion != 4:
             datos_sub_opcion = datos_by_name(sub_opcion)
             #Crea un objeto "Servers" con los datos recibidos
             data_actual = Servers(name=datos_sub_opcion[0],ip=datos_sub_opcion[1],username=datos_sub_opcion[2],password=datos_sub_opcion[3])
-            
-            #Actualizar IP?
-            print(f'¿Deseas actualizar la ip > {data_actual.ip}?\n1. Sí\n2. No')
-            ip = int(input('> '))
-            if ip == 1:
-                new_ip = input(f'Ingresa la nueva IP de >')
-            elif ip == 2:
-                new_ip = data_actual.ip
-            else:
-                os.system('clear')
-                print('OPCCION INCORRECTA')
 
-            #Actualizar USERNAME?
-            print(f'¿Deseas actualizar el usuario > {data_actual.username}?\n1. Sí\n2. No')
-            ip = int(input('> '))
-            if ip == 1:
-                new_username = input(f'Ingresa el nuevo usuario >')
-            elif ip == 2:
-                new_username = data_actual.username
-            else:
-                print('OPCCION INCORRECTA')
-
-            #Actualizar PASSWORD?
-            print(f'¿Deseas actualizar la contraseña de {new_username}?\n1. Sí\n2. No')
-            ip = int(input('> '))
-            if ip == 1:
-                new_password = input(f'Ingresa la nueva contraseña >')
-            elif ip == 2:
-                new_password = data_actual.password
-            else:
-                print('OPCCION INCORRECTA')
-            
-            #Almacena los cambios en un nuevo objeto "Servers"
-            data_actualizada = Servers(data_actual.name, new_ip, new_username,new_password)
-            #Realiza un execute para guardas los cambios
-            ServersDao.actualizar(data_actualizada)
+            actualizar(data_actual)
+            x = input('>')
         else:
             pass
-    
+
     #SALIR DEL PROGRAMA
     elif opcion == 0:
         print('Bye')
