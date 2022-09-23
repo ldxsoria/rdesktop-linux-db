04adc76633c63d4d466a678bc8a2ae4c280c47ff
ldxsoria <ldxsoria@gmail.com>
2022-09-23 07:29:07 -0500

    Update

--- init.py
+++ init.py
@@ -34,18 +34,15 @@ def ejecutar_conexion(sub_opcion):
     #print(remote_server)
 
 def actualizar(data_actual):
-
     pregunta_ip = Etapa(1, 'la ip','la nueva ip', data_actual.ip)
     pregunta_username = Etapa(2, 'el usuario','el nuevo usuario', data_actual.username)
     pregunta_password = Etapa(3, 'la contraseña', 'la nueva contraseña', data_actual.password)
     tupla = (pregunta_ip, pregunta_username, pregunta_password)
-    eleccion = 'ejecutar'
 
     etapa = 0
-
-    
+  
     for e in range(0,3):
-
+        os.system('clear')
         print(f'¿Deseas actualizar {tupla[etapa].pregunta1} > {tupla[etapa].respuesta} ?\n1. Sí\n2. No')
         #Actualizar IP?
         ip = int(input('> '))
@@ -68,71 +65,78 @@ def actualizar(data_actual):
     ServersDao.actualizar(data_actualizada)
 #FIN actualizar()
 
+########################INICIO##########################
+if __name__ == '__main__':
 
-opcion = None
-while opcion != 4:
-    #Este sera el menu principal
-    os.system('clear')
-    print('RDESKTOP DATABASE\nOpciones:')
-    print('1) Conectarme a un equipo')
-    print('2) Agregar una nueva conexion')
-    print('3) Actualizar una conexion existente')
-    print('4) Eliminar una conexion existente')
-    print('0) Salir')
-    opcion = int(input('Ingresa tu opción (0-4) > '))
+    opcion = None
 
-    #Menu 1
-    if opcion == 1:
+    while opcion != 4:
+        #Este sera el menu principal
         os.system('clear')
-        print('Los equipos registrados hasta ahora son:')
-        dic_equipos = equipos_registrados()
-        sub_opcion = int(input('Ingresa el número del equipo a conectar (0 para salir) > '))
-        if sub_opcion != 0:
-            ejecutar_conexion(sub_opcion)
+        print('RDESKTOP DATABASE\nOpciones:')
+        print('1) Conectarme a un equipo')
+        print('2) Agregar una nueva conexion')
+        print('3) Actualizar una conexion existente')
+        print('4) Eliminar una conexion existente')
+        print('0) Salir')
+        opcion = int(input('Ingresa tu opción (0-4) > '))
+
+        #Menu 1
+        if opcion == 1:
+            os.system('clear')
+            print('Los equipos registrados hasta ahora son:')
+            dic_equipos = equipos_registrados()
+            sub_opcion = int(input('Ingresa el número del equipo a conectar (0 para salir) > '))
+            if sub_opcion != 0:
+                ejecutar_conexion(sub_opcion)
+            else:
+                pass
+
+        #Menu 2 REGISTRAR
+        elif opcion == 2:
+            os.system('clear')
+            print('Los equipos registrados hasta ahora son:')
+            equipos_registrados()
+            print('INGRESA "0" PARA REGRESAR AL MENU\nPresiona "ENTER" para continuar')
+            sub_opcion = input('> ')
+            
+            if sub_opcion == '0':
+                pass
+            else:
+                name = input('Ingresa el nombre del equipo >')
+                ip = input(f'Ingresa la IP de {name} >')
+                username = input('Ingresa el usuario > ')
+                password = input('Ingresa la constraseña >')
+                newRegistro = Servers(name, ip, username, password)
+                ServersDao.intertar(newRegistro)
+                print('Servidor registrado')
+
+
+        #Menu 3 ACTUALIZAR
+        elif opcion == 3:
+            os.system('clear')
+            dic_equipos = equipos_registrados()
+            print('0. PARA SALIR')
+            sub_opcion = int(input('Ingresa el número de la conexion a actualizar (0 para salir) > '))
+
+            if sub_opcion != 0:
+
+                #busca la info segun la opcion
+                datos_sub_opcion = datos_by_name(sub_opcion)
+
+                #Crea un objeto "Servers" con los datos recibidos
+                data_actual = Servers(name=datos_sub_opcion[0],ip=datos_sub_opcion[1],username=datos_sub_opcion[2],password=datos_sub_opcion[3])
+
+                actualizar(data_actual)
+                x = input('>')
+            else:
+                pass
+
+        #SALIR DEL PROGRAMA
+        elif opcion == 0:
+            print('Bye')
+            break
         else:
-            pass
-
-    #Menu 2 REGISTRAR
-    elif opcion == 2:
-        os.system('clear')
-        print('Los equipos registrados hasta ahora son:')
-        equipos_registrados()
-        print('INGRESA "0" PARA REGRESAR AL MENU\nPresiona "ENTER" para continuar')
-        sub_opcion = input('> ')
-        if sub_opcion == '0':
-            pass
-        else:
-            name = input('Ingresa el nombre del equipo >')
-            ip = input(f'Ingresa la IP de {name} >')
-            username = input('Ingresa el usuario > ')
-            password = input('Ingresa la constraseña >')
-            newRegistro = Servers(name, ip, username, password)
-            ServersDao.intertar(newRegistro)
-            print('Servidor registrado')
-
-
-    #Menu 3 ACTUALIZAR
-    elif opcion == 3:
-        os.system('clear')
-        dic_equipos = equipos_registrados()
-        sub_opcion = int(input('Ingresa el número de la conexcion a actualizar (0 para salir) > '))
-
-        if sub_opcion != 0:
-            #busca la info segun la opcion
-            datos_sub_opcion = datos_by_name(sub_opcion)
-            #Crea un objeto "Servers" con los datos recibidos
-            data_actual = Servers(name=datos_sub_opcion[0],ip=datos_sub_opcion[1],username=datos_sub_opcion[2],password=datos_sub_opcion[3])
-
-            actualizar(data_actual)
-            x = input('>')
-        else:
-            pass
-
-    #SALIR DEL PROGRAMA
-    elif opcion == 0:
-        print('Bye')
-        break
-    else:
-        os.system('clear')
-        print('>>>> INGRESA UNA OPCION VALIDA <<<<<<<')
-        
+            os.system('clear')
+            print('>>>> INGRESA UNA OPCION VALIDA <<<<<<<')
+            
